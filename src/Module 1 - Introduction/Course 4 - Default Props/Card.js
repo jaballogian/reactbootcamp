5d5ea8d4fa9e6eead2d726b6d1049b54227704b6
jaballogian <jaballogianrma77@gmail.com>
2020-08-31 16:55:46 +0700

    Module 1 - Introduction/Course 4 - Default Props - version 2 (way to use default props in class based component) https://scrimba.com/p/pvwk4Hw/cw88MbTn

--- src/Module 1 - Introduction/Course 4 - Default Props/Card.js
+++ src/Module 1 - Introduction/Course 4 - Default Props/Card.js
@@ -1,22 +1,43 @@
 import React from 'react'
 
 // way to use default props in function based component
-function Card(props){
-    const styles = {
-        backgroundColor: props.cardColor,
-        height: props.height,
-        width: props.width
+// function Card(props){
+//     const styles = {
+//         backgroundColor: props.cardColor,
+//         height: props.height,
+//         width: props.width
+//     }
+
+//     return(
+//         <div style={styles}></div>
+//     )
+// }
+
+// Card.defaultProps = {
+//     cardColor: "blue",
+//     height: 100,
+//     width: 100
+// }
+
+// way to use props in class based component
+class Card extends React.Component{
+    static defaultProps = {
+        cardColor: "blue",
+        height: 100,
+        width: 100
     }
 
-    return(
-        <div style={styles}></div>
-    )
-}
+    render(){
+        const styles = {
+            backgroundColor: this.props.cardColor,
+            height: this.props.height,
+            width: this.props.width 
+        }
 
-Card.defaultProps = {
-    cardColor: "blue",
-    height: 100,
-    width: 100
+        return(
+            <div style={styles}></div>
+        )
+    }
 }
 
 export default Card
