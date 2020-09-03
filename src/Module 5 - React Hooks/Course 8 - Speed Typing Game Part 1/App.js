cd8429fdeacb0ae1374a14ff5efe57b50e9a08b2
jaballogian <jaballogianrma77@gmail.com>
2020-09-03 23:05:21 +0700

    Module 5 - React Hooks/Course 8 - Speed Typing Game Part 1 https://scrimba.com/p/pMvMEuD/cqQMJKh4

---
+++ src/Module 5 - React Hooks/Course 8 - Speed Typing Game Part 1/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Styles.css'
+
+function App(){
+    return(
+        <div>
+            <h1>How fast do you type?</h1>
+            <textarea/>
+            <h4>Time remaining: ???</h4>
+            <button>Start</button>
+            <h1>Word count: ???</h1>
+        </div>
+    )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App'; // default react App module
 // change this to import own react module
-import App from './Module 5 - React Hooks/Course 6 - useEffect() Part 2/App'
+import App from './Module 5 - React Hooks/Course 8 - Speed Typing Game Part 1/App'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
