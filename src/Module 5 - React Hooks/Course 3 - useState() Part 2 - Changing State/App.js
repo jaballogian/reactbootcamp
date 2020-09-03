2dc36ddda4b50c53fe09d02e380c6d664f92e0ee
jaballogian <jaballogianrma77@gmail.com>
2020-09-03 20:17:29 +0700

    Module 5 - React Hooks/Course 3 - useState() Part 2 - Changing State https://scrimba.com/p/pMvMEuD/cWQw9JH2

---
+++ src/Module 5 - React Hooks/Course 3 - useState() Part 2 - Changing State/App.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react'
+
+function App(){
+    const [count, setCount] = useState(0)
+
+    function increment(){
+        setCount((previousCount) => {
+            return(
+                previousCount = previousCount + 1
+            )
+        })
+    }
+
+    function decrement(){
+        setCount((previousCount) => {
+            return(
+                previousCount = previousCount - 1
+            )
+        })
+    }
+    
+    return(
+        <div>
+            <h1>{count}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    )
+}
+
+export default App
