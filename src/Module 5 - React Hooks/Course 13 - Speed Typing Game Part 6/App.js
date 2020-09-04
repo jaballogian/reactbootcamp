ea8827912a87bebb93b2964f8d1baa50703017ae
jaballogian <jaballogianrma77@gmail.com>
2020-09-04 17:46:43 +0700

    Module 5 - React Hooks/Course 13 - Speed Typing Game Part 6 https://scrimba.com/p/pMvMEuD/cpDGRgh8

---
+++ src/Module 5 - React Hooks/Course 13 - Speed Typing Game Part 6/App.js
@@ -0,0 +1,70 @@
+import React, {useState, useEffect} from 'react'
+import './Styles.css'
+
+function App(){
+    const STARTING_TIME = 5
+
+    const [text, setText] = useState("")
+    const [timeRemaining, setTimeRemaining] = useState(5)
+    const [isTimeRunning, setIsTimeRunning] = useState(false)
+    const [wordCount, setWordCount] = useState(0)
+
+    function handleOnChange(event) {
+        const {value} = event.target
+        return(
+            setText(value)
+        )
+    }
+
+    function calculateWordCount(inputText){
+        const wordsArray = inputText.trim().split(" ")
+        return (
+            wordsArray.filter(word => word !== "").length
+        )
+    }
+
+    function startGame(){
+        setIsTimeRunning(true)
+        setTimeRemaining(STARTING_TIME)
+        setText("")
+    }
+
+    function endGame(){
+        setIsTimeRunning(false)
+        setWordCount(calculateWordCount(text))
+    }
+
+    useEffect(() => {
+        if(isTimeRunning && timeRemaining > 0){
+            setTimeout(() => {
+                setTimeRemaining(time => time - 1)
+            }, 1000)
+        }
+        else if(timeRemaining === 0){
+            endGame()
+        }
+    }, [timeRemaining, isTimeRunning])
+
+    return(
+        <div>
+            <h1>How fast do you type?</h1>
+
+            <textarea
+                onChange={handleOnChange}
+                value={text}
+                disabled={!setIsTimeRunning}/>
+
+            <h4>Time remaining: {timeRemaining}</h4>
+
+            <button
+                onClick={startGame}
+                disabled={isTimeRunning}
+                >Start
+            </button>
+
+            <h1>Word count: {wordCount}</h1>
+        </div>
+    )
+}
+
+export default App
