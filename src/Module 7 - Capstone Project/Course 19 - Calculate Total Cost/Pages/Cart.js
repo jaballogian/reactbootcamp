b447429edf10ae01faf5788f077ae4b6376e6fe6
jaballogian <jaballogianrma77@gmail.com>
2020-09-07 21:59:29 +0700

    Module 7 - Capstone Project/Course 19 - Calculate Total Cost https://scrimba.com/p/pBwJrS7/c67rNeup

---
+++ src/Module 7 - Capstone Project/Course 19 - Calculate Total Cost/Pages/Cart.js
@@ -0,0 +1,28 @@
+import React, {useContext} from 'react'
+import {Context} from '../Context'
+import CartItem from '../Components/CartItem'
+
+function Cart(){
+    const {cartItems} = useContext(Context)
+    const totalCost = 5.99 * cartItems.length
+    const totalCostDisplay = totalCost.toLocaleString("en-US", {style: "currency", currency: "USD"})
+
+    const cartItemElements = cartItems.map((item) => {
+        return(
+            <CartItem key={item.id} item={item}/>
+        )
+    })
+
+    return(
+        <main className="cart-page">
+            <h1>Check out</h1>
+            {cartItemElements}
+            <p className="total-cost">Total cost: {totalCostDisplay}</p>
+            <div className="order-button">
+                <button>Place order</button>
+            </div>
+        </main>
+    )
+}
+
+export default Cart
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App'; // default react App module
 // change this to import own react module
-import App from './Module 7 - Capstone Project/Course 18 - Remove Items from Cart Page/Index'
+import App from './Module 7 - Capstone Project/Course 19 - Calculate Total Cost/Index'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
