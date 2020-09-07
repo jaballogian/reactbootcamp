7a970930c314b52c4a20aafdbfc95da4253872b6
jaballogian <jaballogianrma77@gmail.com>
2020-09-07 21:19:13 +0700

    Module 7 - Capstone Project/Course 16 - Update Header Cart Icon https://scrimba.com/p/pBwJrS7/cRq2rLcD

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App'; // default react App module
 // change this to import own react module
-import App from './Module 7 - Capstone Project/Course 15 - Remove From Cart/Index'
+import App from './Module 7 - Capstone Project/Course 16 - Update Header Cart Icon/Index'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
