e25a05bf86cf77399ec96bea69fab5a8c6a2e5dc
jaballogian <jaballogianrma77@gmail.com>
2020-09-09 18:01:17 +0700

    Module 8 - Redux/Course 23 + 24 - Redux in React - useSelector() + Redux in React - useDispatch() https://scrimba.com/p/prpqBCx/cdNgrPTr + https://scrimba.com/p/prpqBCx/c8rGRdhQ

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App'; // default react App module
 // change this to import own react module
-import App from './Module 8 - Redux/Course 22 - Redux in React - mapDispatchToProps/Index'
+import App from './Module 8 - Redux/Course 23 + 24 - Redux in React - useSelector() + Redux in React - useDispatch()/Index'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
